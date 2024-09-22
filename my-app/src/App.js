87bbe77adf091c631f714b38cf4669bab1b3f563
Florian Hass <f91hama@gmail.com>
2024-09-22 09:29:35 +0000

    Add posts to HTTPRequest.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HTTPRequests from './components/HTTPRequest';
 
 function App() {
   return (
     <div>
-      <HTTPRequest />
+      <HTTPRequests />
     </div>
   );
 }
